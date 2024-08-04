37331a2bafa621807705b96e180e8f673468114e
Ethan <ethan@doerhoff.me>
2024-08-03 21:42:25 -0500

    Initial Commit

---
+++ TestTabs/TestTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTabsApp.swift
+//  TestTabs
+//
+//  Created by Ethan Doerhoff on 2024-08-03.
+//
+
+import SwiftUI
+
+@main
+struct TestTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
