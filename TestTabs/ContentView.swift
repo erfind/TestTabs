3681ceef9c82cab82a8579057a69f0a379ec8510
Ethan <ethan@doerhoff.me>
2024-08-07 17:06:18 -0500

    Pushing Application

--- TestTabs/ContentView.swift
+++ TestTabs/ContentView.swift
@@ -8,16 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedIndex = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("\(selectedIndex)")
+        TabView(selection: $selectedIndex) {
+            Text("Home Page")
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+                .tag(0)
+            Text("Setting Page")
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+                .tag(1)
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search")
+                }
+                .tag(2)
+            Text("Share")
+                .tabItem {
+                    Image(systemName: "square.and.arrow.up.fill")
+                    Text("Share")
+                }
+                .tag(3)
+            Text("Trash")
+                .tabItem {
+                    Image(systemName: "trash")
+                    Text("Trash")
+                }
+                .tag(4)
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
